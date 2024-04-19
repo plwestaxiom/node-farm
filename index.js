538de9b3b3220692053de66583a6bef084ddfbde
Paul L. West <paul.west@redx.com>
2024-04-19 09:59:18 +0900

    minor refactor

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const url = require('url');
+const { replaceTemplate } = require('./modules/replace-template');
 
 
 
@@ -27,23 +28,6 @@ const url = require('url');
 const PORT = 3500;
 const data = fs.readFileSync(path.join(__dirname, 'dev-data', 'data.json'), 'utf-8');
 const productDataObj = JSON.parse(data);
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-  return output;
-}
-
 const templateOverview = fs.readFileSync(path.join(__dirname, 'templates', 'template-overview.html'), 'utf-8');
 const templateProduct = fs.readFileSync(path.join(__dirname, 'templates', 'template-product.html'), 'utf-8');
 const templateCard = fs.readFileSync(path.join(__dirname, 'templates', 'template-card.html'), 'utf-8');
